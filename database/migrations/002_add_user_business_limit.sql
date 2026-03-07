d1607f53f90f8d462f6bc11e317c6c161b55a341
FAYnim <yuana6898@gmail.com>
2026-03-07 19:54:43 +0700

    feat: remove migrations

--- database/migrations/002_add_user_business_limit.sql
+++
@@ -1,4 +0,0 @@
--- Migration 002: Add business_limit column to users table
-ALTER TABLE users 
-ADD COLUMN IF NOT EXISTS business_limit INT DEFAULT NULL COMMENT 'NULL = use default/unlimited'
-AFTER password_hash;
--- database/migrations/003_optimize_business_indexes.sql
+++
@@ -1,6 +0,0 @@
--- Migration 003: Optimize business query indexes
-ALTER TABLE businesses 
-ADD INDEX IF NOT EXISTS idx_user_is_primary (user_id, is_primary);
-
-ALTER TABLE businesses 
-ADD INDEX IF NOT EXISTS idx_business_name (business_name);
